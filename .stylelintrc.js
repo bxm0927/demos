bb6694b271f499d2b02d38ce490a1a3b8cd71183
xiaoming.bai <80583600@qq.com>
2021-01-29 14:08:21 +0800

    config

---
+++ .stylelintrc.js
@@ -0,0 +1,19 @@
+/*
+ * Stylelint Configuration
+ * https://github.com/stylelint/stylelint
+ *
+ * @Author: xiaoming.bai
+ * @Date: 2020-09-01 17:54:47
+ * @Last Modified by: xiaoming.bai
+ * @Last Modified time: 2021-01-29 14:05:22
+ */
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-order'],
+  rules: {},
+}
