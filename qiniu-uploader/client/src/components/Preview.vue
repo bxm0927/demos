08906dfa7bcb4fa20e386e78b38470e7a295f9f2
xiaoming.bai <80583600@qq.com>
2021-03-05 11:49:11 +0800

    file api test

---
+++ qiniu-uploader/client/src/components/Preview.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="preview-wrapper">
+    <img
+      v-for="(item, index) in previewList"
+      :key="index"
+      class="pic"
+      :src="item.url"
+      :alt="item.name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Preview',
+  props: {
+    previewList: {
+      type: Array,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.preview-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.preview-wrapper .pic {
+  box-sizing: border-box;
+  width: 112px;
+  height: 112px;
+  margin-top: 10px;
+  margin-right: 10px;
+  object-fit: contain;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+}
+
+.preview-wrapper .pic:nth-child(5n) {
+  margin-right: 0;
+}
+</style>
--- qiniu-uploader/client/src/components/Uploader.vue
+++ qiniu-uploader/client/src/components/Uploader.vue
@@ -2,19 +2,97 @@
   <div class="page">
     <h1 class="title">使用七牛云上传文件</h1>
 
-    <div id="uploader" class="uploader-wrapper">
+    <div
+      class="uploader-wrapper"
+      @dragenter="onFileDragenter"
+      @dragover="onFileDragover"
+      @dragleave="onFileDragleave"
+      @drop="onFileDrop"
+      @paste="onFilePaste"
+    >
       <div class="uploader-content">
-        <input class="uploader-input" type="file" />
-        <button class="uploader-btn">点击选择照片</button>
-        <p class="tips">您可以点击上面的按钮，也可以将图片拖放或粘贴到这里</p>
+        <input
+          id="fileInput"
+          type="file"
+          multiple
+          accept="image/*"
+          style="display: none"
+          @change="onFileChange"
+        />
+        <button class="uploader-btn" type="button" @click="upload">点击选择照片</button>
+        <p class="uploader-tips">您可以点击上面的按钮，也可以将图片拖放或粘贴到这里</p>
       </div>
     </div>
+
+    <Preview v-if="previewList && previewList.length" :preview-list="previewList" />
   </div>
 </template>
 
 <script>
+import Preview from './Preview.vue'
+
 export default {
   name: 'Uploader',
+  components: {
+    Preview,
+  },
+  data() {
+    return {
+      fileList: [],
+      previewList: [],
+    }
+  },
+  methods: {
+    upload() {
+      document.querySelector('#fileInput').click()
+    },
+    handelFiles(files) {
+      this.fileList.push(...files)
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i]
+        const reader = new FileReader()
+
+        reader.readAsDataURL(file)
+        reader.addEventListener('load', (e) => {
+          this.previewList.push({
+            name: file.name,
+            url: e.target.result,
+          })
+        })
+      }
+    },
+
+    onFileChange(e) {
+      this.handelFiles(e.target.files)
+    },
+    onFilePaste(e) {
+      this.handelFiles(e.clipboardData.files)
+    },
+    onFileDragenter(e) {
+      console.log('onFileDragenter')
+      e.stopPropagation()
+      e.preventDefault()
+    },
+    onFileDragover(e) {
+      console.log('onFileDragover')
+      e.stopPropagation()
+      e.preventDefault()
+      document.querySelector('.uploader-content').classList.add('active')
+    },
+    onFileDragleave(e) {
+      console.log('onFileDragleave')
+      e.stopPropagation()
+      e.preventDefault()
+      document.querySelector('.uploader-content').classList.remove('active')
+    },
+    onFileDrop(e) {
+      console.log('onFileDrop')
+      e.stopPropagation()
+      e.preventDefault()
+      this.handelFiles(e.dataTransfer.files)
+      document.querySelector('.uploader-content').classList.remove('active')
+    },
+  },
 }
 </script>
 
@@ -34,6 +112,7 @@ export default {
   padding: 30px 20px;
   background-color: #fafafa;
   border: 1px solid #e5e5e5;
+  border-radius: 4px;
   box-shadow: inset 0 3px 6px rgb(0 0 0 / 5%);
 }
 
@@ -43,6 +122,12 @@ export default {
   background: url('../assets/image.png') no-repeat;
   background-position: center 40px;
   border: 3px dashed #e6e6e6;
+  border-radius: 4px;
+  transition: all 0.3s;
+}
+
+.uploader-content.active {
+  border-color: #74b51c;
 }
 
 .uploader-input {
@@ -63,7 +148,7 @@ export default {
   cursor: pointer;
 }
 
-.tips {
+.uploader-tips {
   text-align: center;
 }
 </style>
